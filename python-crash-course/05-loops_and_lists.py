f36e5eb98026d65ae12e42e0ab805ada948828c2
Andrea <siizx@live.it>
2024-07-07 23:58:50 +0200

    general update

--- python-crash-course/05-loops_and_lists.py
+++ python-crash-course/05-loops_and_lists.py
@@ -16,4 +16,37 @@ for magician in magicians:
     print(magician)
 # cioe', per ogni elemento generico, che chiameremo 'magician', che sta nella lista 'magicians' esegui print(magician)
 
-##### Riprendere da pag 50: A closer look at looping
+numbers = list(range(1,6))
+print(numbers)
+# Output: [1, 2, 3, 4, 5]
+
+numbers = list(range(0,11,2)) # from 0 to 10(11-1) with step 2, so even numbers only 
+print(numbers)
+
+squares = [] # empty list
+for value in range(1,11):
+    squares.append(value**2)
+print(squares)    
+print(f"max(squares): {max(squares)}")
+print(f"sum(squares): {sum(squares)}")
+
+# List Comprehension: pag 59 - praticamente tutto condensatissimo
+cubes = [value**3 for value in range(1,11)]
+print(cubes)
+
+print(cubes[2:5])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
